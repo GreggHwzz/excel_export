55a3f95c4863edc90a527e97ab78569c5101d799
GreggHwzz <gtallaapro@gmail.com>
2024-03-06 18:17:29 +0100

    push 2e jet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import * as XLSX from 'xlsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AddToExcel extends React.Component {
+  addToExcel = () => {
+    // Charger le fichier Excel existant
+    const fileReader = new FileReader();
+    fileReader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: 'array' });
+
+      // Modifier le fichier Excel en ajoutant de nouvelles données
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const newData = [
+        ['New Name', 'New Age'],
+        ['Mike', 35],
+        ['Emily', 28]
+      ];
+      const range = XLSX.utils.decode_range(worksheet['!ref']);
+      range.e.r += newData.length - 1;
+      XLSX.utils.sheet_add_aoa(worksheet, newData, { origin: -1 });
+
+      // Sauvegarder le fichier Excel mis à jour
+      const updatedData = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+      const blob = new Blob([updatedData], { type: 'application/octet-stream' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'updated_data.xlsx');
+      document.body.appendChild(link);
+      link.click();
+    };
+
+    // Charger le fichier existant (vous devez avoir un input[type=file] dans votre JSX)
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.xlsx';
+    input.onchange = (event) => {
+      const file = event.target.files[0];
+      fileReader.readAsArrayBuffer(file);
+    };
+    input.click();
+  };
+
+  render() {
+    return (
+      <button onClick={this.addToExcel}>
+        Add to Excel
+      </button>
+    );
+  }
 }
 
-export default App;
+export default AddToExcel;
